2cb8d817c6a58fe64679d805f5cbd5e5efc3f957
Julian Fuentes <142046690+Seillamirtilico@users.noreply.github.com>
2023-08-15 14:09:36 -0500

    Add files via upload
    Se agrega modulo y potencia

---
+++ Calculadora.py
@@ -0,0 +1,33 @@
+# Lectura del valor de 2 variables enteras por consola:
+print("Ingrese el numero 1")
+numero1 = int(input())
+print("Ingrese el número 2")
+numero2 = int(input())
+print("Ingrese la operacion (+, -, *, /)")
+operacion = input()
+
+match operacion:
+    case '+':
+        # Operación suma:
+        suma = numero1 + numero2
+        print("La suma es " + str(suma))
+    case '-':
+        # Operación resta:
+        resta = numero1 - numero2
+        print("La resta es " + str(resta))
+    case '*':
+        # Operación multiplicación:
+        multiplicacion = numero1 * numero2
+        print("La multiplicación es " + str(multiplicacion))
+    case '/':
+        # Operación división:
+        division = numero1 / numero2
+        print("La división es " + str(division))
+    case 'p':
+        potencia= pow(numero1,numero2)
+        print("La potencia es "+ str(potencia))
+    case '%':
+        modulo= numero1 % numero2
+        print("el modulo es "+ str(modulo))
+    case _ :
+        print("Operación inválida")
